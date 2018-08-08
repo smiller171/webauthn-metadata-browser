10a8ac9e4cb15ae7c00a78f1c3410da85831d817
Scott Miller <scott@millergeek.xyz>
2018-08-08 13:23:09 +0000

    sort entries

--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,10 @@
       v-if="mdsData.mdsCollections"
     >
       <card
-        v-for="(entry, key) in mdsData.mdsCollections[0]['fido-mds1'].entries"
+        v-for="(entry, key) in entriesArray(mdsData.mdsCollections[0]['fido-mds1'].entries)"
         :key="key"
       >
-        <h2>{{ key }}</h2>
-        <h3>{{ entry.description }}</h3>
+        <h2>{{ entry.description }}</h2>
         <img 
           :src="entry.icon"
           alt=""
@@ -50,7 +49,10 @@ export default {
   methods: {
     latestStatus: (arr) => arr.slice().sort(
       (a,b) => a.effectiveDate>b.effectiveDate?-1:1
-    )[0]
+    )[0],
+    entriesArray: (obj) => Object.values(obj).sort(
+      (a,b) => a.description.toLowerCase()>b.description.toLowerCase()?1:-1 
+    )
   }
 }
 </script>
