103293cfd837ff4fe87471f1c38680856b33c289
Scott Miller <scott@millergeek.xyz>
2018-08-09 13:37:16 +0000

    make cards white

--- components/Card.vue
+++ components/Card.vue
@@ -9,6 +9,8 @@
 <style lang="scss" scoped>
 .card {
   box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
-  margin: 1rem;
+  padding: 1rem;
+  margin-top: 1rem;
+  background-color: white;
 }
 </style>
