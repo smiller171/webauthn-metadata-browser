eec4f7f3d85dc8142156e8581c1e710a5683e3ca
Scott Miller <scott@millergeek.xyz>
2018-08-24 10:18:09 -0400

    minimal test page

---
+++ pages/test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-if="!toggle">
+      First view
+    </div>
+    <div v-if="toggle">
+      Second view
+    </div>
+    <input
+      id="toggle" 
+      v-model="toggle"
+      type="checkbox"
+      name="toggle"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      toggle: null
+    }
+  }
+}
+</script>
